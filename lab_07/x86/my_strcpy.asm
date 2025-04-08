197f5d51b425e6ecb0de51ee15545d008bc878f9
Nikita <zhni23u172@student.bmstu.ru>
2025-04-08 13:52:04 +0300

    FIXME: Overlapping buffers

--- lab_07/x86/my_strcpy.asm
+++ lab_07/x86/my_strcpy.asm
@@ -14,7 +14,19 @@ my_strcpy:
     mov esi, dword [ebp+12] ; src
     ; mov dword [rbp-8], edi
     mov ecx, dword [ebp+16] ; size
+    lea eax, [edi+ecx]
+    cmp eax, esi
+    ja .fwd
+
+    cmp edi, esi
+    jb .fwd
+    std
+    lea esi, [esi+ecx-1]
+    lea edi, [edi+ecx-1]
+
+.fwd:
     rep movsb
+    cld
 
     mov byte [edi], 0 ; Терминирующий '\0'
 
